3e8e9be3567c3b1257f1c7449a598ca29159bcb0
Charles <charlesn_pinel@hotmail.com>
2018-05-05 15:38:21 -0400

    Popur aide avec Will pour Json

--- app/src/main/java/hackqc2018/org/infopoubelle/CollecteActivity.java
+++ app/src/main/java/hackqc2018/org/infopoubelle/CollecteActivity.java
@@ -1,16 +1,31 @@
 package hackqc2018.org.infopoubelle;
 
 import android.graphics.Color;
+import android.content.res.AssetManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 
+import org.json.JSONObject;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+
 public class CollecteActivity extends AppCompatActivity {
+
+    ArrayList<String> autres = new ArrayList<>();
+    ArrayList<String> compost = new ArrayList<>();
+    ArrayList<String> dechets = new ArrayList<>();
+    ArrayList<String> ecocentre = new ArrayList<>();
+    ArrayList<String> recuperation = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,55 +37,37 @@ public class CollecteActivity extends AppCompatActivity {
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
 
         dropdown.setAdapter(adapter);
-
-        init();
-
+        dropdown.setOnItemSelectedListener(new YourItemSelectedListener());
     }
+public class YourItemSelectedListener implements AdapterView.OnItemSelectedListener {
 
-    public void init() {
-        TableLayout stk = (TableLayout) findViewById(R.id.tabInfo);
-        TableRow tbrow0 = new TableRow(this);
-        TextView tv0 = new TextView(this);
-        tv0.setText(" Sl.No ");
-        tv0.setTextColor(Color.BLACK);
-        tbrow0.addView(tv0);
-        TextView tv1 = new TextView(this);
-        tv1.setText(" Product ");
-        tv1.setTextColor(Color.BLACK);
-        tbrow0.addView(tv1);
-        TextView tv2 = new TextView(this);
-        tv2.setText(" Unit Price ");
-        tv2.setTextColor(Color.BLACK);
-        tbrow0.addView(tv2);
-        TextView tv3 = new TextView(this);
-        tv3.setText(" Stock Remaining ");
-        tv3.setTextColor(Color.BLACK);
-        tbrow0.addView(tv3);
-        stk.addView(tbrow0);
-        for (int i = 0; i < 25; i++) {
-            TableRow tbrow = new TableRow(this);
-            TextView t1v = new TextView(this);
-            t1v.setText("" + i);
-            t1v.setTextColor(Color.BLACK);
-            t1v.setGravity(Gravity.CENTER);
-            tbrow.addView(t1v);
-            TextView t2v = new TextView(this);
-            t2v.setText("Product " + i);
-            t2v.setTextColor(Color.BLACK);
-            t2v.setGravity(Gravity.CENTER);
-            tbrow.addView(t2v);
-            TextView t3v = new TextView(this);
-            t3v.setText("Rs." + i);
-            t3v.setTextColor(Color.BLACK);
-            t3v.setGravity(Gravity.CENTER);
-            tbrow.addView(t3v);
-            TextView t4v = new TextView(this);
-            t4v.setText("" + i * 15 / 32 * 10);
-            t4v.setTextColor(Color.BLACK);
-            t4v.setGravity(Gravity.CENTER);
-            tbrow.addView(t4v);
-            stk.addView(tbrow);
+    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+        String selected = parent.getItemAtPosition(pos).toString();
+        JSONObject obj;
+        if (selected.equals("Laval")) {
+            //obj = new JSONObject(loadLaval());
+        } else {
+            //obj = new JSONObject(loadSherbrooke());
         }
-
     }
+
+    public void onNothingSelected(AdapterView parent) {
+    }
+
+    public String loadLaval()
+    {
+
+        String json = null;
+
+        return "";
+    }
+
+    public String loadSherbrooke()
+    {
+        return "";
+    }
+}
+
+
+
 }
