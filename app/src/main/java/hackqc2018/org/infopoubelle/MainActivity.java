c22d7a5685c301ed6c0708b37fdef4c3370fee41
Charles <charlesn_pinel@hotmail.com>
2018-05-05 02:46:25 -0400

    fenêtre de connexion

--- app/src/main/java/hackqc2018/org/infopoubelle/MainActivity.java
+++ app/src/main/java/hackqc2018/org/infopoubelle/MainActivity.java
@@ -1,14 +1,30 @@
 package hackqc2018.org.infopoubelle;
 
+import android.content.Intent;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    final String nomCitoyent = "denis";
+    final String mdpCitoyent = "Skynet";
+    final String nomVille    = "Mike";
+    final String mdpVille    = "Beatrice";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+    }
+
+    public void connexion(View view){
+        //TextInputLayout nomUsage = findViewById(R.id.nomUsage);
+        //TextInputLayout mdpUsage = findViewById(R.id.mdpUsage);
+        //if((nomUsage.getEditText().getText().toString() == nomCitoyent) && (mdpUsage.getEditText().getText().toString() == mdpCitoyent)) {
+            Intent intent = new Intent(this, AccueilActivity.class);
+            startActivity(intent);
+       // }
     }
 }
