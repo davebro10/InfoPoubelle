abf574673ca567ca3d9dfaa350ec5e9b7e6251ab
Charles <charlesn_pinel@hotmail.com>
2018-05-05 14:32:41 -0400

    désolé Kathy.... :(

--- app/src/main/java/hackqc2018/org/infopoubelle/CollecteActivity.java
+++ app/src/main/java/hackqc2018/org/infopoubelle/CollecteActivity.java
@@ -2,6 +2,8 @@ package hackqc2018.org.infopoubelle;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class CollecteActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class CollecteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.collecte_activity);
+        Spinner dropdown = findViewById(R.id.droplist);
+        String[] items = new String[]{"Laval", "Sherbrooke"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+
+        dropdown.setAdapter(adapter);
     }
 }
