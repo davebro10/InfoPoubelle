2219deb63680d83cabbf6a544a191c56ae9c8732
Charles <charlesn_pinel@hotmail.com>
2018-05-05 08:34:55 -0400

    Ajout du fichier Collecte

---
+++ app/src/main/java/hackqc2018/org/infopoubelle/CollecteActivity.java
@@ -0,0 +1,13 @@
+package hackqc2018.org.infopoubelle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CollecteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.collecte_activity);
+    }
+}
