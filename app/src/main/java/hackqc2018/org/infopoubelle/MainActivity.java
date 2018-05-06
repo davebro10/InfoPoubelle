b2a957af74a801c4a8b3b7e3b892fe9b9f1312a5
Charles <charlesn_pinel@hotmail.com>
2018-05-06 08:21:20 -0400

    On voie la ligne des mot de passe

--- app/src/main/java/hackqc2018/org/infopoubelle/MainActivity.java
+++ app/src/main/java/hackqc2018/org/infopoubelle/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,26 +17,32 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        TextView erreur = findViewById(R.id.error35);
         setContentView(R.layout.main_activity);
     }
 
     public void connexion(View view){
         TextInputEditText nomUsage = findViewById(R.id.nomUsage);
         TextInputEditText mdpUsage = findViewById(R.id.mdpUsage);
+        TextView erreur = findViewById(R.id.error35);
         if((nomUsage.getText().toString().equals(nomCitoyent) && mdpUsage.getText().toString().equals(mdpCitoyent))){
             Intent intent = new Intent(this, AccueilActivity.class);
             intent.putExtra("nom", nomUsage.getText().toString());
             intent.putExtra("type", false); //n'est pas un employé de la ville
+            erreur.setVisibility(view.GONE);
             startActivity(intent);
         }
         else if(nomUsage.getText().toString().equals(nomVille) && mdpUsage.getText().toString().equals(mdpVille) ){
             Intent intent = new Intent(this, AccueilActivity.class);
             intent.putExtra("nom", nomUsage.getText().toString());
             intent.putExtra("type", true); //est un employé de la ville
+            erreur.setVisibility(view.GONE);
             startActivity(intent);
         }
         else{
 
+            erreur.setText("Nom d'usagé ou mot de passe invalide");
+            erreur.setVisibility(view.VISIBLE);
         }
     }
 }
